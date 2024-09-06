144b0097160ce9c40a3e022103c8af3d76dc6df8
XHenriqueX <henriquerique233@gmail.com>
2024-09-06 15:52:28 -0300

    Add files via upload

--- Lista Kleisson/filtrandoPropriedade/js/system.js
+++ Lista Kleisson/filtrandoPropriedade/js/system.js
@@ -1,22 +1,13 @@
-let pessoa = {
-    nome: "João",
-    idade: 25,
-    profissao: "Desenvolvedor",
-    salario: 5000
-};
+
 
 function enter() {
-    let in1 = document.getElementById("nome").value;
-    let in2 = document.getElementById("idade").value;
-    let in3 = document.getElementById("profissao").value;
-    let in4 = document.getElementById("salario").value;
     let novoObj = {
-        nome: in1,
-        idade: in2,
-        profissao: in3,
-        salario: in4
+        nome: document.getElementById("nome").value,
+        idade: document.getElementById("idade").value,
+        profissao: document.getElementById("profissao").value,
+        salario: document.getElementById("salario").value
     };
     let criterio = document.getElementById("criterio").value;
-    document.getElementById("resultado").value = novoObj
+    console.log(novoObj.criterio)
 }
 
